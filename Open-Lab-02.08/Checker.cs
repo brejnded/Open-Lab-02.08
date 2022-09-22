18a1c4797b08a038bf6e95b960e58d14f535aa2d
Matúš Nekoranec <113455013+brejnded@users.noreply.github.com>
2022-09-22 08:33:56 +0200

    Update Checker.cs

--- Open-Lab-02.08/Checker.cs
+++ Open-Lab-02.08/Checker.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Open_Lab_02._08
 {
@@ -6,6 +6,7 @@ namespace Open_Lab_02._08
     {
         public bool IsEmpty(string str)
         {
+            return str == null || str.Length == 0;
             throw new NotImplementedException();
         }
     }
